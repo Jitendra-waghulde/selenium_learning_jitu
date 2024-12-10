da825a6ead71190a4440715f8fe46fb5af73fd9a
Jitendra-waghulde <73056177+Jitendra-waghulde@users.noreply.github.com>
2024-12-11 04:50:58 +0530

    ok

---
+++ JavaStart/src/main/java/DataTypes1.java
@@ -0,0 +1,13 @@
+public class DataTypes1 {
+    public static void main(String[] args) {
+        byte b1 = 10;
+        System.out.println(b1);
+
+        short s1 = 180;
+        System.out.println(s1);
+
+//        Casting concept
+        byte n1 = (byte) s1;
+        System.out.println(n1);
+    }
+}
